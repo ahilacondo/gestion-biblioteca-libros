af1a462195857a05888040b287180447a7e020af
ahilacondo <ahilacondo@unsa.edu.pe>
2024-07-18 00:14:37 -0500

    Add gui folder to .gitignore

--- gui/build/Desktop_Qt_6_7_2_llvm_mingw_64_bit-Debug/.qt/QtDeployTargets.cmake
+++
@@ -1,3 +0,0 @@
-set(__QT_DEPLOY_TARGET_gui_FILE C:/2024-UNSA/2024 - A/PROGRAMACION DE SISTEMAS/gestion-biblioteca-libros/gui/build/Desktop_Qt_6_7_2_llvm_mingw_64_bit-Debug/gui.exe)
-set(__QT_DEPLOY_TARGET_gui_TYPE EXECUTABLE)
-set(__QT_DEPLOY_TARGET_gui_RUNTIME_DLLS C:/Qt/6.7.2/llvm-mingw_64/bin/Qt6Widgets.dll;C:/Qt/6.7.2/llvm-mingw_64/bin/Qt6Gui.dll;C:/Qt/6.7.2/llvm-mingw_64/bin/Qt6Core.dll)
